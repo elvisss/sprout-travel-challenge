4b7eec106b98dd495c10261064476d2bb1ba34ae
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-03-02 22:51:02 -0500

    adding components to homeview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,13 @@
 
-import FlightOption from '@/components/FlightOption.vue';
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <div>
-      <FlightOption />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import FlightOption from '@/components/FlightOption.vue';
 </script>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <h1>hello</h1>
-  </main>
+  <div class="container">
+    <h1>Flights List</h1>
+
+    <FlightItineraries :itineraries="itineraries" />
+
+    <!-- Pagination -->
+    <PaginationNumber
+      :total-pages="totalPages"
+      :current-page="currentPage"
+      @page-changed="setPage"
+    />
+
+  </div>
+  <!-- Loading -->
+  <LoadingModal v-if="isLoading" />
 </template>
+
+<script setup lang="ts">
+import FlightItineraries from '@/components/FlightItineraries/ItineraryList.vue'
+import PaginationNumber from '@/components/PaginationNumber.vue'
+import LoadingModal from '@/components/LoadingModal.vue'
+import useFlights from '@/composables/useFlights';
+
+const { isLoading, itineraries, totalPages, currentPage, setPage } = useFlights()
+</script>
